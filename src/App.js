b70b31de9db5cf39c6dad5d2c12d2f6647d34262
ivan gasparoni <ivangasparoni@gmail.com>
2021-07-16 20:51:30 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Primer proyecto en React JS
         </a>
       </header>
     </div>
