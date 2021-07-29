f2d101baeb7e00f4ef4ce06df0d0f1d19ee0738a
ivan gasparoni <ivangasparoni@gmail.com>
2021-07-29 00:34:16 -0300

    Creacion de Componente NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,43 @@
+import logo from "../img/coffee.png";
+import "../styles/Index.css";
+
+export default function NavBar () {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                <img className="logo" src={logo} alt="Logo Brewed Beans" />
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link active me-3" aria-current="page" href="#a">Brewed Beans</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link me-3" href="#b">Home Roasters</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link me-3" href="#c"><i className="fas fa-shopping-cart me-2"></i>Tienda</a>
+                    </li>
+                    <li className="nav-item dropdown me-3">
+                        <a className="nav-link dropdown-toggle" href="#d" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Variedades
+                        </a>
+                        <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a className="dropdown-item" href="#e">XXXXXXX</a></li>
+                        <li><a className="dropdown-item" href="#f">XXXXXXXXXX</a></li>
+                        <li><a className="dropdown-item" href="#g">XXXXXXXXXXXXXX</a></li>
+                        </ul>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#h">Contacto</a>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+        </>
+    )
+}
